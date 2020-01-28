b1ef2d17b8ea0aa5f901c0d025965bd623f5c4fb
Prashankumar-Tech <prashanresume@gmail.com>
2020-01-28 15:33:16 +0000

    Add project files.

---
+++ CreateCSVfileFromDB/CSVFileGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateCSVfileFromDB
+{
+    public static class CSVFileGenerator
+    {
+        private static readonly object FileLocker = new object();
+        public static void ToCSV(this DataTable dtDataTable, string strFilePath, bool header, string CSVDelimiter)
+        {
+            lock (FileLocker)
+            {
+                System.IO.FileInfo file = new System.IO.FileInfo(strFilePath);
+                file.Directory.Create();
+                using (StreamWriter sw = new StreamWriter(strFilePath, false, new UTF8Encoding(false)))
+                {
+                    //headers  
+                    if (header)
+                    {
+                        for (int i = 0; i < dtDataTable.Columns.Count; i++)
+                        {
+                            sw.Write(dtDataTable.Columns[i]);
+                            if (i < dtDataTable.Columns.Count - 1)
+                            {
+                                sw.Write(CSVDelimiter);
+                            }
+                        }
+                        sw.Write(sw.NewLine);
+                    }
+                    foreach (DataRow dr in dtDataTable.Rows)
+                    {
+                        for (int i = 0; i < dtDataTable.Columns.Count; i++)
+                        {
+                            if (!Convert.IsDBNull(dr[i]))
+                            {
+                                string value = dr[i].ToString().Trim();
+                                if (value.Contains(','))
+                                {
+                                    value = String.Format("\"{0}\"", value);
+                                    sw.Write(value);
+                                }
+                                else
+                                {
+                                    sw.Write(dr[i].ToString().Trim());
+                                }
+                            }
+                            if (i < dtDataTable.Columns.Count - 1)
+                            {
+                                sw.Write(CSVDelimiter);
+                            }
+                        }
+                        sw.Write(sw.NewLine);
+                    }
+                    sw.Close();
+                }
+            }
+        }
+    }
+
+}
---
+++ CreateCSVfileFromDB/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateCSVfileFromDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlConnectionString = ConfigurationManager.ConnectionStrings["EmployeeDataContext"].ConnectionString;
+            string CSVDelimiter = ConfigurationManager.AppSettings["CSVDelimiter"];
+            string CSVFileNameEndsWith = ConfigurationManager.AppSettings["CSVFileNameEndsWith"];
+            string CSVFilePath = ConfigurationManager.AppSettings["CSVFilePath"];
+            DateTime dt = DateTime.UtcNow;
+            string extension1 = dt.ToString("dd/MM/yyyy").Replace("/", "");
+            string extension2 = dt.ToString("hh/mm/ss").Replace("/", "");
+            string extension3 = CSVFileNameEndsWith ?? "Consultation";
+            string inputZipFileName = CSVFilePath + "\\" + extension1 + "_" + extension2 + "_" + extension3 + ".zip";
+            string employeeFile = CSVFilePath + "\\" + extension1 + "_" + extension2 + "_" + extension3 + "_KPI.csv";
+            string inputEmployeeTypeFileName = CSVFilePath + "\\" + extension1 + "_" + extension2 + "_" + extension3 + "_Type.csv";
+            CreateCSVFilesAndZipIt(sqlConnectionString, employeeFile, inputEmployeeTypeFileName,  CSVDelimiter, inputZipFileName);
+        }
+
+        private static bool CreateCSVFilesAndZipIt(string sqlConnectionString, string employeeFile, string inputEmployeeTypeFileName, string CSVDelimiter, string inputZipFileName)
+        {
+            bool success = false;
+            Console.WriteLine("Inside CreateCSVFilesAndZipIt Method");
+
+            using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+            {
+                try
+                {
+                    using (var cmd = new SqlCommand("GetEmployeeReport", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        using (SqlDataAdapter dataAdapter
+                        = new SqlDataAdapter(cmd))
+                        {
+                            // create the DataSet 
+                            DataSet dataSet = new DataSet();
+                            // fill the DataSet using our DataAdapter 
+                            dataAdapter.Fill(dataSet);
+                            DataTable dataTableKPI = dataSet.Tables[0];
+                            dataTableKPI.ToCSV(employeeFile, false, CSVDelimiter);
+                            DataTable dataTableEmployeeType = dataSet.Tables[1];
+                            dataTableEmployeeType.ToCSV(inputEmployeeTypeFileName, false, CSVDelimiter);
+                            Console.WriteLine("Employee Type CSV file generated");
+
+                            using (ZipArchive zip = ZipFile.Open(inputZipFileName, ZipArchiveMode.Create))
+                            {
+                                zip.CreateEntryFromFile(employeeFile, Path.GetFileName(employeeFile));
+                                zip.CreateEntryFromFile(inputEmployeeTypeFileName, Path.GetFileName(inputEmployeeTypeFileName));
+                            }
+                            success = true;
+                            Console.WriteLine("Zip file generated");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Zip file not generated:" + ex.Message);
+                    success = false;
+
+                    return success;
+                }
+                return success;
+            }
+        }
+    }
+}
